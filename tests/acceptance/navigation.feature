caa6496d0f6b3ed414dd0d4d624ca87a69eeaeae
Natalia Matvieieva <ne.matveeva@gmail.com>
2019-08-28 11:46:37 +0300

    Add Acceptance tests

--- tests/acceptance/navigation.feature
+++ tests/acceptance/navigation.feature
@@ -1,7 +1,13 @@
-Feature: Test navigation betwen pages
+Feature: Test navigation between pages
   Navigation description
 
   Scenario: Homepage can go to Blog
     Given I am on a homepage
-    When I click on the link with id "blog-link"
+    When I click on the "Go to blog" link
     Then I am on the blog page
+
+
+  Scenario: Blog can go to Homepage
+    Given I am on a blog page
+    When I click on the "Go to home" link
+    Then I am on the homepage
--- tests/acceptance/steps/navigation.py
+++ tests/acceptance/steps/navigation.py
@@ -1,17 +1,33 @@
 from behave import *
 from selenium import webdriver
 
+from tests.acceptance.page_model.blog_page import BlogPage
+from tests.acceptance.page_model.home_page import HomePage
+
 use_step_matcher('re')
 
 
 @given('I am on a homepage')
 def step_impl(context):
-    context.browser = webdriver.Chrome('chromedriver')
-    context.browser.get('http://127.0.0.1:5000')
+    context.driver = webdriver.Chrome()
+    page = HomePage(context.driver)
+    context.driver.get(page.url)
+
+
+@given('I am on a blog page')
+def step_impl(context):
+    context.driver = webdriver.Chrome()
+    page = BlogPage(context.driver)
+    context.driver.get(page.url)
 
 
 @then('I am on the blog page')
 def step_impl(context):
-    expected_url = 'http://127.0.0.1:5000/blog'
-    assert context.browser.current_url == expected_url
-    
+    expected_url = BlogPage(context.driver).url
+    assert context.driver.current_url == expected_url
+
+
+@then('I am on the homepage')
+def step_impl(context):
+    expected_url = HomePage(context.driver).url
+    assert context.driver.current_url == expected_url
