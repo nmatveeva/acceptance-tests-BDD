70102ceb588e7b5b0dcdae94f9d91be2e7065b01
Natalia Matvieieva <ne.matveeva@gmail.com>
2019-08-28 15:03:44 +0300

    Add waits in tests

--- tests/acceptance/steps/navigation.py
+++ tests/acceptance/steps/navigation.py
@@ -3,6 +3,7 @@ from selenium import webdriver
 
 from tests.acceptance.page_model.blog_page import BlogPage
 from tests.acceptance.page_model.home_page import HomePage
+from tests.acceptance.page_model.new_post_page import NewPostPage
 
 use_step_matcher('re')
 
@@ -20,6 +21,11 @@ def step_impl(context):
     page = BlogPage(context.driver)
     context.driver.get(page.url)
 
+@given('I am on the new post page')
+def step_impl(context):
+    context.driver = webdriver.Chrome()
+    page = NewPostPage(context.driver)
+    context.driver.get(page.url)
 
 @then('I am on the blog page')
 def step_impl(context):
